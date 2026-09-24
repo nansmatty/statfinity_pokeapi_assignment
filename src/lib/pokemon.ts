1bab5791790be2c859385a8d69dad8965380a23c
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-09-24 17:38:14 +0530

    Refactor getPokemonList to return names as strings and update return type

--- src/lib/pokemon.ts
+++ src/lib/pokemon.ts
@@ -1,16 +1,17 @@
-import { PokemonDetail, PokemonListResponse } from '@/types/pokemon';
+import { PokemonCardData, PokemonDetail, PokemonListItem } from '@/types/pokemon';
 
 const BASE_URL = 'https://pokeapi.co/api/v2';
 const TOTAL_POKEMON = 1351;
 
-export const getPokemonList = async (): Promise<PokemonListResponse> => {
+export const getPokemonList = async (): Promise<string[]> => {
 	const res = await fetch(`${BASE_URL}/pokemon?limit=${TOTAL_POKEMON}&offset=0`, {
 		cache: 'force-cache',
 	});
 	if (!res.ok) {
 		throw new Error('Failed to fetch Pokemon list');
 	}
-	return res.json();
+	const data = await res.json();
+	return data.results.map((item: { name: string }) => item.name);
 };
 
 export const getPokemonByName = async (name: string): Promise<PokemonDetail> => {
@@ -37,4 +38,23 @@ export const getPokemonByName = async (name: string): Promise<PokemonDetail> => 
 			},
 		},
 	};
+};
+
+export const getPokemonListHomepage = async (): Promise<PokemonCardData[]> => {
+	const res = await fetch(`${BASE_URL}/pokemon?limit=${TOTAL_POKEMON}&offset=0`, {
+		cache: 'force-cache',
+	});
+	if (!res.ok) {
+		throw new Error('Failed to fetch Pokemon list for homepage');
+	}
+	const data = await res.json();
+	return data.results.map((item: PokemonListItem) => {
+		const id = Number(item.url.split('/').filter(Boolean).pop());
+
+		return {
+			id: id,
+			name: item.name,
+			image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+		};
+	});
 };
