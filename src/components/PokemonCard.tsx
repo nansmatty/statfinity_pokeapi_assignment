e538acd618cb1ddd8c5c70ff5aa872418278bd65
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-09-24 23:19:13 +0530

    Replace button with Link component for navigation in PokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,5 @@
 import { PokemonCardData } from '@/types/pokemon';
+import Link from 'next/link';
 
 interface PokemonCardProps {
 	pokemon: PokemonCardData;
@@ -14,9 +15,11 @@ const PokemonCard = ({ pokemon }: PokemonCardProps) => {
 				<p className='text-sm text-gray-500'>#{pokemon.id.toString().padStart(3, '0')}</p>
 				<h2 className='text-sm font-semibold capitalize tracking-wider'>{pokemon.name}</h2>
 			</div>
-			<button className='mt-4 cursor-pointer w-full rounded-lg text-sm font-semibold tracking-wider bg-blue-500 py-2 text-white hover:bg-blue-600 transition-colors duration-300'>
+			<Link
+				href={`/pokemon/${pokemon.name}`}
+				className='mt-4 block w-full rounded-lg bg-blue-500 py-2 text-center text-sm font-semibold tracking-wider text-white transition-colors duration-300 hover:bg-blue-600'>
 				View Details
-			</button>
+			</Link>
 		</div>
 	);
 };
