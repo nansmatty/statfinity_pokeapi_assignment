c90b7d3f80dc2a93b2791cf62122965c480c647e
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-09-24 17:12:03 +0530

    Add cache option to fetch in getPokemonList function

--- src/lib/pokemon.ts
+++ src/lib/pokemon.ts
@@ -4,7 +4,9 @@ const BASE_URL = 'https://pokeapi.co/api/v2';
 const TOTAL_POKEMON = 1351;
 
 export const getPokemonList = async (): Promise<PokemonListResponse> => {
-	const res = await fetch(`${BASE_URL}/pokemon?limit=${TOTAL_POKEMON}&offset=0`);
+	const res = await fetch(`${BASE_URL}/pokemon?limit=${TOTAL_POKEMON}&offset=0`, {
+		cache: 'force-cache',
+	});
 	if (!res.ok) {
 		throw new Error('Failed to fetch Pokemon list');
 	}
