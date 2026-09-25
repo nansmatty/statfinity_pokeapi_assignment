b7d57a889017fc50b2bdb6cc3ed316e5dfd7fd24
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-09-25 13:07:40 +0530

    Adjust grid layout in PokemonExplorer for consistent column display

--- src/components/PokemonExplorer.tsx
+++ src/components/PokemonExplorer.tsx
@@ -7,7 +7,7 @@ interface PokemonExplorerProps {
 
 const PokemonExplorer = ({ pokemonList }: PokemonExplorerProps) => {
 	return (
-		<div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-6 p-4'>
+		<div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 p-4'>
 			{pokemonList.map((pokemon) => (
 				<PokemonCard key={pokemon.name} pokemon={pokemon} />
 			))}
