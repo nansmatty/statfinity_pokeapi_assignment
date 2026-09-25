9908d8d1a563808b1759b8232c0a3650d51b97d4
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-09-25 12:44:45 +0530

    Enhance PokemonCard component with improved styling and detail display

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,5 @@
 import { PokemonCardData } from '@/types/pokemon';
+import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 
 interface PokemonCardProps {
@@ -9,16 +10,16 @@ const PokemonCard = ({ pokemon }: PokemonCardProps) => {
 	return (
 		<div className='rounded-xl border border-gray-200 bg-white p-4 shadow-sm hover:shadow-lg transition-shadow duration-300'>
 			<div className='flex justify-center rounded-lg bg-gray-50 p-4'>
-				<img src={pokemon.image} alt={pokemon.name} className='h-32 w-32 object-contain' />
+				<img src={pokemon.image} alt={pokemon.name} className='h-32 w-32 object-contain scale-150' />
 			</div>
 			<div className='mt-4'>
 				<p className='text-sm text-gray-500'>#{pokemon.id.toString().padStart(3, '0')}</p>
-				<h2 className='text-sm font-semibold capitalize tracking-wider'>{pokemon.name}</h2>
+				<h2 className='font-extrabold capitalize tracking-wider'>{pokemon.name}</h2>
 			</div>
 			<Link
 				href={`/pokemon/${pokemon.name}`}
-				className='mt-4 block w-full rounded-lg bg-blue-500 py-2 text-center text-sm font-semibold tracking-wider text-white transition-colors duration-300 hover:bg-blue-600'>
-				View Details
+				className='mt-4 block w-full rounded-lg bg-blue-200 py-2 text-center text-sm font-bold tracking-wider text-blue-800 transition-colors duration-300 hover:bg-blue-300 hover:text-blue-900'>
+				View Details <ArrowRight className='inline-block ml-2 h-4 w-4' />
 			</Link>
 		</div>
 	);
